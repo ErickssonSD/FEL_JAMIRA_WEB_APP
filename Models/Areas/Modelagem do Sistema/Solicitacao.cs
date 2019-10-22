e295bbd785221ee6ece66a11ebc252ba73242518
Ericksson Souza Drozda <erickssondrozda@outlook.com>
2019-10-22 19:30:14 -0300

    fefefef

---
+++ Models/Areas/Modelagem do Sistema/Solicitacao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FEL_JAMIRA_WEB_APP.Models.Areas.Modelagem_do_Sistema
+{
+    public class Solicitacao
+    {
+        public Double ValorTotal { get; set; }
+        public Double ValorTotalEstacionamento { get; set; }
+        public Double ValorGanho { get; set; }
+        public int Status { get; set; }
+        public int IdCliente { get; set; }
+        public int IdEstacionamento { get; set; }
+    }
+}
